e24d873881a052e92cfb58de4fe6d798f5dfd496
Omar Razzack <oarazzack@ucdavis.edu>
2022-01-27 11:07:01 -0800

    Initializing React App with webpack

---
+++ App/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './index.css'
+
+function App() {
+  render() {
+    return()
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
